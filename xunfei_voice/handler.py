77ba5bb01a6e4f0a292db4b889e9e78ae75047b4
artisticzhao <hkcreedblacktea@hotmail.com>
2018-08-21 15:51:12 +0800

    增加了指令接口

--- xunfei_voice/handler.py
+++ xunfei_voice/handler.py
@@ -1,13 +1,17 @@
 # coding:utf-8
 import serial
-from settings import SERI_TO_SK, SERI_TO_YY
+from settings import SERI_TO_YY
+'''
+Start kit 接收的是语音模块的串口2指令，
+使用语音模块的串口转发功能进行转发
+'''
 
 
 class Voice_Ctrl_Handler(object):
     def __init__(self):
         try:
             # use usb serial port
-            self.ser = serial.Serial(SERI_TO_SK, 9600, timeout=0.5)
+            self.ser = serial.Serial(SERI_TO_YY, 9600, timeout=0.5)
         except Exception as e:
             print e
             self.ser = None
@@ -16,14 +20,14 @@ class Voice_Ctrl_Handler(object):
         if self.ser is not None:
             if text.find(u'向左看') != -1:
                 print 'left'
-                self.ser.write('l')
+                self.ser.write('@Retransmit#*IR^001&$')
                 return True
             elif text.find(u'向右看') != -1:
                 print 'right'
-                self.ser.write('r')
+                self.ser.write('@Retransmit#*IR^002&$')
                 return True
             elif text.find(u'向前看') != -1:
                 print 'forward'
-                self.ser.write('f')
+                self.ser.write('@Retransmit#*IR^003&$')
                 return True
         return False
