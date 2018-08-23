a553ba4bffac90edd93589ee8e57305e13185a0b
artisticzhao <hkcreedblacktea@hotmail.com>
2018-08-23 20:28:08 +0800

    加入语音调用录入人脸功能

--- xunfei_voice/handler.py
+++ xunfei_voice/handler.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-from settings import UP2_IP, MAIN_PORT
+from settings import UP2_IP, MAIN_PORT, FACE_TRIGGER_PORT
 from socketer import socket_sender
 '''
 Start kit 接收的是语音模块的串口2指令，
@@ -10,6 +10,7 @@ Start kit 接收的是语音模块的串口2指令，
 class Voice_Ctrl_Handler(object):
     def __init__(self):
         self.sender_to_main = socket_sender(UP2_IP, MAIN_PORT)
+        self.sender_to_trigger = socket_sender(UP2_IP, FACE_TRIGGER_PORT)
 
     def switch(self, text):
         if text.find(u'向左看') != -1:
@@ -23,4 +24,8 @@ class Voice_Ctrl_Handler(object):
         elif text.find(u'向前看') != -1:
             print 'forward'
             self.sender_to_main.send_data('motor:3')
+            return True
+        elif text.find(u'录入人脸') != -1:
+            print 'rec face'
+            self.sender_to_trigger.send_data('start:getface')
             return True
